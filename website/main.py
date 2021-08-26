d854f18250ae7bd40de2a0f63cf35da5b25de253
K4liber <janbielecki94@gmail.com>
2021-08-26 20:05:05 +0200

    add marker from db

--- website/main.py
+++ website/main.py
@@ -1,11 +1,9 @@
 import os
-from dataclasses import dataclass
-from typing import Optional, List
 
 import flask
 from flask import render_template
 import requests
-
+import ast
 
 ASSETS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
 app = flask.Flask(__name__, template_folder=ASSETS_DIR, static_folder=ASSETS_DIR)
@@ -13,31 +11,36 @@ app.config["DEBUG"] = True
 api_url = os.getenv('API_URL')
 
 
-@dataclass
-class Location:
-    longitude: float
-    latitude: float
-
-
-@dataclass
-class DeviceData:
-    location: Optional[Location] = None
-
-
-@dataclass
-class Data:
-    devices: Optional[List[DeviceData]] = None
-
-
 @app.route("/")
 def mapview():
-    try:
-        response = requests.get(api_url)
-        app.logger.info(response.text)
-    except BaseException as be:
-        app.logger.warning(f'API exception: {be}')
+    data = dict()
+    url = api_url + '/get_localization'
+    app.logger.info(f'Trying to hit (GET) the URL: {url}')
 
-    return render_template('map.html', data=Data())
+    try:
+        localization_response = requests.get(url)
+        localization_json = ast.literal_eval(localization_response.text)
+        app.logger.info(localization_json)
+    except BaseException as be:
+        localization_json = None
+        app.logger.warning(f'API exception: {be}')
+        data['error'] = str(be)
+
+    if localization_json:
+        try:
+            data['devices'] = [
+                {
+                    'localization': {
+                        'longitude': float(localization_json['lon']) if 'lon' in localization_json else 0.0,
+                        'latitude': localization_json['lat'] if 'lat' in localization_json else 0.0,
+                    }
+                }
+            ]
+        except BaseException as be:
+            data['error'] = str(be)
+
+    app.logger.info(f'Data: {data}')
+    return render_template('map.html', data=data)
 
 
 app.run(host='0.0.0.0')
--- website/static/load_map.js
+++ website/static/load_map.js
@@ -1,4 +1,4 @@
-function showPosition(position) {
+function getBasicMap(position) {
     var mymap = L.map('mapid').setView([position.coords.latitude, position.coords.longitude], 13);
 
     var tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiazRsaWJlciIsImEiOiJja3NtczE4MmUwMW9jMnBucDZkdWYyZ2JzIn0.bRAZ1jLsbV1tY1-zNr9UzA', {
@@ -17,12 +17,28 @@ function showPosition(position) {
         "<img style='width: 80px;' src='static/images/avatar.jpg'/>" +
         "<br>Your device" +
         "</div>");
+    return mymap
+}
+
+function addMarkers(map, data) {
+    for (let i = 0; i < data['devices'].length; i++) {
+        let device = data['devices'][i]
+        let localization = device['localization']
+        let marker = L.marker([localization['latitude'], localization['longitude']]).addTo(map);
+        marker.bindPopup("" +
+            "<div style='margin: 0 auto;'>" +
+            "<img style='width: 80px;' src='static/images/avatar.jpg'/>" +
+            "<br>Jan Bielecki" +
+            "</div>");
+    }
 }
 
 function load_map(data) {
-    // TODO load markers from devices parameter
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition((position) => {
+                let basicMap = getBasicMap(position)
+                addMarkers(basicMap, data)
+            });
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
